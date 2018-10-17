18d12f2fa596cef642d21e5e8be871dc8f3b53bc
Dan Lindeman <lindemda@gmail.com>
2018-10-17 00:25:23 -0400

    WIP progress on swapping rules for 10-16

--- generator/core.pl
+++ generator/core.pl
@@ -19,43 +19,27 @@ wall(none, wall).
 % are players special cases where having many types is okay?
 0 { sprite(T, S, C) : player(C, S)} 1 :- tile(T).
 
-% DEBUG Swapping -- make a wall at y=5
-:- not width {sprite((X, 5), wall, none): dim(5)} width.
-:- not sprite((1,1), player, green).
-:- not sprite((9,10), goal, green).
-:- not sprite((10,10), player, blue).
-:- not sprite((1,2), goal, green).
+% DEBUG Swapping -- make a wall at y=5, good test for swapping reachability
+% :- not width {sprite((X, 5), wall, none): dim(5)} width.
+% :- not sprite((1,1), player, green).
+% :- not sprite((9,10), goal, green).
+% :- not sprite((10,10), player, blue).
+% :- not sprite((2,1), goal, blue).
 
-% There is one goal and one player token for every color
+% There is one goal and one player token for every color -- BUG
 :- not number_of_players { sprite(T, goal, C) } number_of_players.
 :- not number_of_players { sprite(T, player, C) } number_of_players.
 
 % Walls don't overlap goals and characters
 :- sprite(T, wall, C), sprite(T, player, C).
-:- sprite(T, wall, C), sprite(T, player, C).
 :- sprite(T, wall, C), sprite(T, goal, C).
 :- sprite(T, wall, C), sprite(T, door, C).
 
-% players do not start on goals -- DEBUG feature
+% players do not start on goals
 :- sprite(T, player, C1), sprite(T, goal, C2).
-
-% % These might be non-constraints ------------
-
-% % Players are not adjacent to goals
-% 0 { sprite(T1,green_character) : adj(T1,T2) } 0 :- sprite(T2,green_goal).
-% 0 { sprite(T1,blue_character) : adj(T1,T2) } 0 :- sprite(T2,blue_goal).
-
-% % Players are not adjacent to each other
-% 0 { sprite(T1,green_character) : adj(T1,T2) } 0 :- sprite(T2,blue_character).
-
-% % Goals are not adjacent to each other
-% 0 { sprite(T1,green_goal) : adj(T1,T2) } 0 :- sprite(T2,blue_goal).
 
 % There are wall sprites in 50% of tiles, doors in 10%
 :- not width*width/2 { sprite(T, wall, C)} width*width/2.
 :- not width*width/2 { sprite(T, door, C)} width*width/2.
-% :- 1 { sprite(T, door, C)} 1.
-
-% --------------------------------------------
 
 #show sprite/3.
--- generator/sim.pl
+++ generator/sim.pl
@@ -1,45 +1,65 @@
-% Tile adjacency
+% Tile adjacency definition
 adj((X1,Y1),(X2,Y2)) :-
     tile((X1,Y1)),
     tile((X2,Y2)),
     |X1-X2| + |Y1-Y2| == 1.
 
-% Players can reach their goal
+% Players touch the first tile they start on
 { touch(T, C) : color(C) } :- sprite(T, player, C).
+
+% The players finish is wherever their same-colored goal is
 { finish(T, C) : color(C) } :- sprite(T, goal, C).
 
 % ------ possible navigation paths -------
 
-% Basic movement reachability
+% Walking rules:
+
+% You can walk on adjacent tiles
 { touch(T2, C): adj(T1,T2) } :- touch(T1, C).
 
+% You can never walk through a wall
+:- sprite(T, wall, none), touch(T, C).
+
+% Door rules:
 % You can never walk through a different colored door
 % I love that doors are a special case of a wall. Doorwall ;) 
-:- sprite(T,door, C1), touch(T, C2), C1 != C2.
+:- sprite(T, door, C1), touch(T, C2), C1 != C2.
 
-% you can never walk through a wall
-:- sprite(T,wall, none), touch(T, C).
+% Swapping rules:
+% This is too broad! It doesn't really capture the temporal nature of swapping.
+% That is, just because I have ever been able to get to my goal
+% doesn't mean that I can end up there at the same time as every other player
+{ touch((X2, Y2), C1) } :- 
+    touch((X1, Y1), C1), touch((X2, Y2), C2), X1=X2, Y1!=Y2, C1!=C2.
 
-% TODO Touching is possible if a tile I have touched
-% and a tile another player has touch share an X or Y.
-{ touch((X1, Y1), C1);touch((X2, Y2), C2) } :- 
-    touch((X1, Y1), C1), touch((X2, Y2), C2), X1=X2.
+{ touch((X2, Y2), C2) } :- 
+    touch((X1, Y1), C1), touch((X2, Y2), C2), Y1=Y2, X1!=X2, C1!=C2.
 
-{ touch((X1, Y1), C1);touch((X2, Y2), C2) } :- 
-    touch((X1, Y1), C1), touch((X2, Y2), C2), Y1=Y2.
+move(T, C) :- touch(T, C).
 
-% { touch((X2, Y2), C1) } :- 
-%     touch((X1, Y1), C1), touch((X2, Y2), C2), X1=X2.
+swap(X1, Y1, C1, X2, Y2, C2) :- touch((X1, Y1), C1), 
+                                touch((X2, Y2), C2), 
+                                Y1=Y2, 
+                                X1!=X2, 
+                                C1!=C2.
 
-% { touch((X2, Y2), C1) } :- 
-%     touch((X1, Y1), C1), touch((X2, Y2), C2), Y1=Y2.
+swap(X1, Y1, C1, X2, Y2, C2) :- touch((X1, Y1), C1), 
+                                touch((X2, Y2), C2), 
+                                Y1!=Y2, 
+                                X1=X2, 
+                                C1!=C2.
 % -----------------------------------------
 
-
+% This is the problem. Completion isn't merely touching your goal
+% Completion should be touching your goal at the same time as everyone else
 completed(C) :- finish(T, C), touch(T, C).
 
-% the finish tile must be touched by each color token
-:- not number_of_players { completed(C) : color(C) } number_of_players.
-:- number_of_moves {touch(T,C): color(C)} number_of_moves.
+% Reject any level where the finish tile was not touched by each color token
+:- not number_of_players { completed(C) : color(C), player(C, player) }.
+
+% The number of spaces touched by every player must equal number_of_moves -- BUG
+:- not number_of_moves * number_of_players {touch(T,C): color(C), player(C, player)} number_of_moves * number_of_players.
 
 #show touch/2.
+% #show swap/6.
+% #show move/2.
