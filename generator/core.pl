5c42ac1e54e71dd65a2fe00d6aa01fea896b95b6
Dan Lindeman <lindemda@gmail.com>
2018-10-30 11:00:43 -0400

    Ensure overlapping tiles are not produced, fixes 'too few goals/players' bug

--- generator/core.pl
+++ generator/core.pl
@@ -5,30 +5,26 @@
 dim(1..width).
 tile((X,Y)) :- dim(X), dim(Y).
 
-number_of_players { color(green;blue) } number_of_players.
-goal(C, goal) :- color(C).
+number_of_players { color(green;red;blue;yellow) } number_of_players.
 door(C, door) :- color(C).
-player(C, player) :- color(C).
 wall(none, wall).
+goal(C, goal) :- color(C).
+player(C, player) :- color(C).
+goal(C, goal) :- sprite(T, goal, C).
+player(C, player) :- sprite(T, player, C).
 
 % tiles have at most one non-floor type 
 0 { sprite(T, S, C) : goal(C, S)} 1 :- tile(T).
 0 { sprite(T, S, C) : door(C, S)} 1 :- tile(T).
 0 { sprite(T, S, C) : wall(C, S)} 1 :- tile(T).
 
-% are players special cases where having many types is okay?
+% are players special cases where having many types is okay? Future work.
 0 { sprite(T, S, C) : player(C, S)} 1 :- tile(T).
 
-% DEBUG Swapping -- make a wall at y=5, good test for swapping reachability
-% :- not width {sprite((X, 5), wall, none): dim(5)} width.
-% :- not sprite((1,1), player, green).
-% :- not sprite((9,10), goal, green).
-% :- not sprite((10,10), player, blue).
-% :- not sprite((2,1), goal, blue).
+:- { sprite(T, goal, C) } != number_of_players.
+:- { sprite(T, player, C) } != number_of_players.
+:- sprite(T1, player, C1), sprite(T2, player, C2), C1==C2, T1!=T2.
 
-% There is one goal and one player token for every color
-1 { sprite(T, goal, C): tile(T) } 1 :- goal(C, goal).
-1 { sprite(T, player, C): tile(T) } 1 :- player(C, player).
 
 % Walls don't overlap goals and characters
 :- sprite(T, wall, C), sprite(T, player, C).
@@ -38,8 +34,8 @@ wall(none, wall).
 % players do not start on goals
 :- sprite(T, player, C1), sprite(T, goal, C2).
 
-% There are wall sprites in 50% of tiles, doors in 25-50%
-:- not width*width/2 { sprite(T, wall, C)} width*width/2.
-:- not width*width/4 { sprite(T, door, C)} width*width/2.
+% Don't put players or goals on doors.
+:- sprite(T, player, C1), sprite(T, door, C2).
+:- sprite(T, goal, C1), sprite(T, door, C2).
 
 #show sprite/3.
