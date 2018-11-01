f809bbf5cdb0bb380df5dc8ce9a0629cceeb547c
Dan Lindeman <lindemda@gmail.com>
2018-11-01 09:13:54 -0400

    add visibility of a goal generation, constraints, uncomment swapping

--- generator/sim.pl
+++ generator/sim.pl
@@ -10,6 +10,9 @@ adj((X1,Y1),(X2,Y2)) :-
 % Players touch the first tile they start on
 { touch(T, C) : color(C) } :- sprite(T, player, C).
 
+% Players see the first tile they start on
+{ see(T, C) : color(C) } :- sprite(T, player, C).
+
 % The players finish is wherever their same-colored goal is
 { finish(T, C) : color(C) } :- sprite(T, goal, C).
 
@@ -28,37 +31,30 @@ adj((X1,Y1),(X2,Y2)) :-
 % I love that doors are a special case of a wall. Doorwall ;) 
 :- sprite(T, door, C1), touch(T, C2), C1 != C2.
 
+% You can see anything adjacent to you, regardless of doors between you.
+{ see(T2, C): adj(T1,T2) } :- see(T1, C).
+
+% You still can't see through walls.
+:- sprite(T, wall, none), see(T, C).
+
+% Don't generate a level where every player can't see a goal. This is 'sees their own goal'
+see_a_goal(C1) :- finish(T, C1), see(T, C2), C1 != C2.
+% see_a_goal(C) :- { see(T, C) }, sprite(T1, player, C), sprite(T, goal, C).
+% { see_a_goal(C): color(C) } == number_of_players.
+
+% Rejects any level where any goal isn't visible to a player -- BUG
+:- player(C, player), not see_a_goal(C).
+:- not number_of_players { see_a_goal(C) : color(C), player(C, player) }.
+
 % Swapping rules:
 % This is too broad! It doesn't really capture the temporal nature of swapping.
 % That is, just because I have ever been able to get to my goal
 % doesn't mean that I can end up there at the same time as every other player
-% { touch((X2, Y2), C1) } :- 
-%     touch((X1, Y1), C1), touch((X2, Y2), C2), X1=X2, Y1!=Y2, C1!=C2.
+{ touch((X2, Y2), C1) } :-
+    touch((X1, Y1), C1), touch((X2, Y2), C2), X1=X2, Y1!=Y2, C1!=C2.
 
-% { touch((X2, Y2), C2) } :- 
-%     touch((X1, Y1), C1), touch((X2, Y2), C2), Y1=Y2, X1!=X2, C1!=C2.
-
-% move(T, C) :- touch(T, C).
-
-% swap(X1, Y1, C1, X2, Y2, C2) :- touch((X1, Y1), C1), 
-%                                 touch((X2, Y2), C2), 
-%                                 Y1=Y2, 
-%                                 X1!=X2, 
-%                                 C1!=C2.
-
-% swap(X1, Y1, C1, X2, Y2, C2) :- touch((X1, Y1), C1), 
-%                                 touch((X2, Y2), C2), 
-%                                 Y1!=Y2, 
-%                                 X1=X2, 
-%                                 C1!=C2.
-% -----------------------------------------
-
-% There has to exist a DAG for this level
-% That is, there is some 
-% start -> { move;swap } -> finish for every player
-% path(C) :- start(T, C),
-%             { move(T, C); swap(X1, Y1, C, X2, Y2, C2) }, % some number of moves and swaps
-%             finish(T, C).
+{ touch((X2, Y2), C2) } :-
+    touch((X1, Y1), C1), touch((X2, Y2), C2), Y1=Y2, X1!=X2, C1!=C2.
 
 % This is the problem. Completion isn't merely touching your goal
 % Completion should be touching your goal at the same time as everyone else
@@ -68,6 +64,7 @@ completed(C) :- finish(T, C), touch(T, C).
 :- not number_of_players { completed(C) : color(C), player(C, player) }.
 
 % The number of spaces touched by every player must equal number_of_moves -- BUG: counts overall total num moves
-:- not {touch(T,C): player(C, player)} == number_of_moves * number_of_players.
+% :- not {touch(T,C): player(C, player)} == number_of_moves * number_of_players.
 
+#show see/2.
 #show touch/2.
