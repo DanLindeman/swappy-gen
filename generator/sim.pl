f178dcba9aaaff3f4c8a4f148450e698f4961422
Dan Lindeman <lindemda@gmail.com>
2018-12-10 14:45:37 -0500

    move rules around so that they are closer to conceptually related rules

--- generator/sim.pl
+++ generator/sim.pl
@@ -1,25 +1,19 @@
+% Players start
+{ start(T, C) : color(C) } :- sprite(T, player, C).
+
+% The players finish is wherever their same-colored goal is
+{ finish(T, C) : color(C) } :- sprite(T, goal, C).
+
+% Players touch the first tile they start on
+{ touch(T, C) : color(C) } :- sprite(T, player, C).
+
 % Tile adjacency definition
 adj((X1,Y1),(X2,Y2)) :-
     tile((X1,Y1)),
     tile((X2,Y2)),
     |X1-X2| + |Y1-Y2| == 1.
 
-% Players start
-{ start(T, C) : color(C) } :- sprite(T, player, C).
-
-% Players touch the first tile they start on
-{ touch(T, C) : color(C) } :- sprite(T, player, C).
-
-% Players see the first tile they start on
-{ see(T, C) : color(C) } :- sprite(T, player, C).
-
-% The players finish is wherever their same-colored goal is
-{ finish(T, C) : color(C) } :- sprite(T, goal, C).
-
-% ------ possible navigation paths -------
-
-% Walking rules:
-
+% Movement rules:
 % You can walk on adjacent tiles
 { touch(T2, C) : adj(T1,T2) } :- touch(T1, C).
 
@@ -46,6 +40,18 @@ adj((X1,Y1),(X2,Y2)) :-
     X1!=X2,
     C1!=C2.
 
+
+% This is the problem. Completion isn't merely touching your goal
+% Completion should be touching your goal at the same time as everyone else
+completed(C) :- finish(T, C), touch(T, C).
+
+% Reject any level where the finish tile was not touched by each color token
+:- not number_of_players { completed(C) : color(C), player(C, player) }.
+
+% Ensuring players start in regions that are not 'goal-locked' 
+% Players see the first tile they start on
+{ see(T, C) : color(C) } :- sprite(T, player, C).
+
 % You can see anything adjacent to you, regardless of doors between you.
 { see(T2, C): adj(T1,T2) } :- see(T1, C).
 
@@ -56,15 +62,5 @@ adj((X1,Y1),(X2,Y2)) :-
 see_a_goal(C) :- finish(T, C2), see(T, C), C != C2.
 :- player(C, player), not see_a_goal(C).
 :- not number_of_players { see_a_goal(C) : color(C), player(C, player) }.
-
-% This is the problem. Completion isn't merely touching your goal
-% Completion should be touching your goal at the same time as everyone else
-completed(C) :- finish(T, C), touch(T, C).
-
-% Reject any level where the finish tile was not touched by each color token
-:- not number_of_players { completed(C) : color(C), player(C, player) }.
-
-% The number of spaces touched by every player must equal number_of_moves -- BUG: counts overall total num moves
-% :- not {touch(T,C): player(C, player)} == number_of_moves * number_of_players.
 
 #show touch/2.
